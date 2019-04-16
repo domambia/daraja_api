8bbfe322acb71b946dd3c06f4e94d73b655e0e34
omambiadauglous <omambiadauglous@gmail.com>
2019-04-16 08:38:04 +0000

    ['chore'] untrcked fils

--- auth/auth.py
+++ auth/auth.py
@@ -8,13 +8,14 @@ from requests.auth import HTTPBasicAuth
 from M2Crypto import RSA, X509
 from base64 import b64encode
 
-from config.credentials import Authenticate
+from config.credentials import Authenticate, AccountBalance
 
 # object creations
 auth = Authenticate()
+account = AccountBalance()
+base_dir = os.path.dirname(os.path.realpath(__file__))
 
 class Authenticate(object):
-
 	"""
 	Authenticate the user:
 	Methods:
@@ -32,5 +33,10 @@ class Authenticate(object):
 		return data['access_token']
 
 	def get_encrypted_password(self):
-		pass
-
+		with open(os.path.join(base_dir, 'cert.cer'), mode = 'r') as f:
+			cert = X509.load_cert_string(f.read()) #reading the file content 
+			#pub_key = X509.load_cert_string(f.read())
+			pub_key = cert.get_pubkey()
+			rsa_key = pub_key.get_rsa()
+			cipher = rsa_key.public_encrypt(bytes(account.security_credential,'utf-8'), RSA.pkcs1_padding)
+			return  b64encode(cipher)
